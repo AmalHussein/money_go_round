5a3313d3f9b4f25012839d6cb7ba2072028a0c8e
Amal Hussein <amalhussein08@gmail.com>
2013-10-25 15:47:31 -0400

    Setup controllers, and models. Added notes to readme file about restoring and backing up database

--- app/models/lender.rb
+++ app/models/lender.rb
@@ -1,2 +1,8 @@
 class Lender < ActiveRecord::Base
+	has_many :borrowers , through: :loans
+
+	def lenders_by_country
+		@lenders = 
+	end 
+	
 end
--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,2 +1,4 @@
 class Loan < ActiveRecord::Base
+	has_many :payments 
+	has_many :lenders 
 end
