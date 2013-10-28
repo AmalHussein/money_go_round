d1add8017945bfaf6e937256cfe9fcd97c20ae01
Amal Hussein <amalhussein08@gmail.com>
2013-10-27 20:00:00 -0400

    playing with more of the views

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -15,16 +15,28 @@ require 'kiva'
 #Methods for my API Calls
 
 def self.recent
-	@newest = Kiva::Loan.load_newest
-	@newest.each do |loan|
-		puts "#{loan.name}"
-		puts "Sector:#{loan.sector}"
-		puts "#{loan.description}"
-		puts "Current Basket Amount:#{loan.basket_amount}"
-		puts "Amount funded so far $#{loan.funded_amount}"
-	end 
-end
+	newest = Kiva::Loan.load_newest
+ 	@name = newest.name
+	@description= newest.description
+	@funded = newest.funded_amount
+
+	# # @newest.each do |loan|
+	# 	puts "#{loan.name}"
+	# 	puts "Sector:#{loan.sector}"
+	# 	puts "#{loan.description}"
+	# 	puts "Current Basket Amount:#{loan.basket_amount}"
+	# 	puts "Amount funded so far $#{loan.funded_amount}"
+	# end 
+# end
+end 
 
 
+# [#<Kiva::Loan:0x007f8ec5db8318 @id=622728, @name="Cecilia", 
+# 	@description={"languages"=>["en"]}, @status="fundraising",
+# 	 @funded_amount=0, @basket_amount=0, @image={"id"=>1455543, "template_id"=>1},
+# 	  @activity="General Store", @sector="Retail", @use="to buy additional stock to 
+# 	  sell in her general store.", @location={"country_code"=>"PH", "country"=>"Philippines",
+# 	  "town"=>"Calatrava, Negros Occidental", "geo"=>{"level"=>"country", "pairs"=>"13 122", 
+# 	  	"type"=>"point"}}, @partner_id=145, @posted_date=2013-10-27 22:30:16 UTC>,
 
 end 
